5bd636e54376b51fcef87aea5ec2e7ce4468fbfe
David Song <david.song0802@gmail.com>
2016-03-12 05:33:55 -0500

    added dickens-filter, extract-rkt to run, ignore; provided sample output

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 { cd pubmed-fetch-rb && ruby main.rb; } | 
-{ cd berkeleyparser && java -jar BerkeleyParser-1.7.jar -gr eng_sm6.gr; } 
+{ cd berkeleyparser && java -jar BerkeleyParser-1.7.jar -gr eng_sm6.gr; } |
+tr -d ".,;" |
+{ cd crea-extract-rkt && racket extract.rkt; } |
+{ cd dickens-filter && ruby main.rb; } |
+cat
 
