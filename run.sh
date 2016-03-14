35e1525b9396d63002791897592a3faa88d780fd
David Song <david.song0802@gmail.com>
2016-03-13 20:04:01 -0400

    added new, larger output; added more to pipeline

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-{ cd pubmed-fetch-rb && ruby main.rb; } | 
+{ cd pubmed-fetch-rb && ruby main.rb; } |
 { cd berkeleyparser && java -jar BerkeleyParser-1.7.jar -gr eng_sm6.gr; } |
-tr -d ".,;" |
+tr -d ".',;[]" |
 { cd crea-extract-rkt && racket extract.rkt; } |
 { cd dickens-filter && ruby main.rb; } |
 cat
